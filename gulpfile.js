8e442215bf2df504fdf246b72a8c615afec86211
cris-brin <cristian74295@gmail.com>
2022-05-18 10:51:08 +0300

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,12 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 const watch = require('gulp-watch');
+const autoprefixer = require('gulp-autoprefixer');
 
+
+/**
+ * Compiler SCSS to CSS 
+ */
 gulp.task('sass-compile', function () {
     return gulp.src('./assets/sass/**/*.scss')
         .pipe(sourcemaps.init())
@@ -11,6 +16,22 @@ gulp.task('sass-compile', function () {
         .pipe(gulp.dest('./assets/css/'));
 });
 
+
+/**
+ * Autoprefixer
+ */
+gulp.task('style-ready', function () {
+
+
+    gulp.src('./assets/css/style.css')
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(gulp.dest('css-ready'));
+
+});
+
 gulp.task('watch', function () {
     gulp.watch('./assets/sass/**/*.scss', gulp.series('sass-compile'));
+    gulp.watch('./assets/sass/**/*.scss', gulp.series('style-ready'));
 });
